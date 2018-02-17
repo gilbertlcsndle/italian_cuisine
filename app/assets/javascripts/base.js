5362ca882cabc906e7bea6972533f70cb2dcee8f
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-02-18 05:46:44 +0800

    Setup javascripts

---
+++ app/assets/javascripts/base.js
@@ -0,0 +1,8 @@
+//= require base/jquery.min
+//= require base/bootstrap
+//= require base/slick
+//= require base/simple-animated-counter
+//= require base/jquery.magnific-popup.min
+//= require base/bootstrap-datepicker
+//= require base/app
+//= require base/custom
