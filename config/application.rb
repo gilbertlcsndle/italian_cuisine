6f679f730ca96a7a93653b539772558042d48202
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-02-18 06:08:24 +0800

    Disable helper, assets, skip routes for generators

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,11 @@ module Osteriax
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.system_tests nil
+      g.helper false
+      g.assets false
+      g.skip_routes true
+    end
   end
 end
