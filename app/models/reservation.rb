44ba3b664f53d3a18de9e26ad63c42bd53a2d9d5
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-06 17:35:26 +0800

    Add reservation model validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,8 @@
 class Reservation < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :phone, presence: true, numericality: { only_integer: true }
+  validates :number_of_guests, presence: true
+  validates :date_time, presence: true
 end
---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe Reservation do
+  let(:reservation) { build(:reservation) }
+
+  it 'has a valid factory' do
+    expect(reservation).to be_valid
+  end
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:phone) }
+  it { should validate_numericality_of(:phone).only_integer }
+  it { should validate_presence_of(:number_of_guests) }
+  it { should validate_presence_of(:date_time) }
+
+  it 'validates the format of email' do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+
+    addresses.each do |invalid_address|
+      reservation.email = invalid_address
+      expect(reservation).not_to be_valid
+    end
+  end
+end
