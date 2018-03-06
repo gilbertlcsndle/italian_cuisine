c60de9d13d7c1b1f4407607ddb9dd4ed94ecc33c
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-06 12:04:11 +0800

    Convert html form to rails form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :subscribers, only: [:create]
   resources :contacts, only: [:create]
+  resources :reservations, only: [:create]
 end
