d43d4e361d19a5585df6aa69f9e3fbb788015ab0
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-09 21:10:42 +0800

    Skip sending email in creating reservation in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Reservation.skip_callback(:create, :after, :send_details_to_admin)
 Reservation.create!(name: 'John Doe', email: 'johndoe@example.com',
                     phone: 123456789, number_of_guests: 50,
                     date_time: 1.month.from_now, message: 'Hello World!')
