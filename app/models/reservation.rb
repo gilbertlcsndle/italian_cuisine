88a38eff427d6e6c77e82f05a4cd85ea7ef16ed5
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-09 21:07:31 +0800

    Move after_create callback to its function in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
-  after_create { ReservationMailer.new_reservation(self).deliver_now }
+  after_create :send_details_to_admin
 
   validates :name, presence: true
   validates :email, presence: true,
@@ -7,4 +7,9 @@ class Reservation < ApplicationRecord
   validates :phone, presence: true, numericality: { only_integer: true }
   validates :number_of_guests, presence: true
   validates :date_time, presence: true
+
+  private
+    def send_details_to_admin
+      ReservationMailer.new_reservation(self).deliver_now
+    end
 end
