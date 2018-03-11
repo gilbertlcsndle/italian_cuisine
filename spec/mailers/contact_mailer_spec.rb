044fe3f3a692efaed59d9bae71533d2902bdb7d6
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-11 14:03:13 +0800

    Add contact mailer test

---
+++ spec/mailers/contact_mailer_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe ContactMailer do
+  let(:contact) { build(:contact) }
+  let(:mail) { ContactMailer.new_message(contact) }
+
+  it 'renders the headers' do
+    expect(mail.subject).to eq(contact.subject)
+    expect(mail.to).to eq(['gilbert.lacasandile@gmail.com'])
+    expect(mail.from).to eq(['noreply@osteriax.com'])
+  end
+
+  it 'renders the body' do
+    expect(mail.body.encoded).to match(contact.name)
+    expect(mail.body.encoded).to match(contact.email)
+    expect(mail.body.encoded).to match(contact.message)
+  end
+end
