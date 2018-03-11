374935bafdf4ed464238a437a9e079ffa95bf027
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-11 16:19:54 +0800

    Add status in Reservation seed, equal to 'Reserved'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 Reservation.skip_callback(:create, :after, :send_details_to_admin)
-Reservation.create!(name: 'John Doe', email: 'johndoe@example.com',
-                    phone: 123456789, number_of_guests: 50,
-                    date_time: 1.month.from_now, message: 'Hello World!')
+Reservation.create!(name: 'John Doe',
+                    email: 'johndoe@example.com',
+                    phone: 123456789,
+                    number_of_guests: 50,
+                    date_time: 1.month.from_now,
+                    message: 'Hello World!',
+                    status: 'Reserved')
