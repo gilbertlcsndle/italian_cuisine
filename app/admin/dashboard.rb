9b6a6f36618e75b293ec1bed3f6a73e01fd11e31
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-30 16:42:21 +0800

    Add items to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,90 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel 'Today' do
+          if Reservation.today.any?
+            table_for Reservation.today.limit(5) do
+              column :name do |reservation|
+                link_to reservation.name, admin_reservation_path(reservation)
+              end
+              column :number_of_guests
+              column 'Date', :date_time, sortable: :date_time do |reservation|
+                reservation.date_time.strftime('%B %-d, %Y %I:%M %P')
+              end
+              column :status do |reservation|
+                case reservation.status
+                when 'Pending'
+                  status_tag reservation.status, class: 'warning'
+                when 'Confirmed'
+                  status_tag reservation.status, class: 'ok'
+                when 'Closed'
+                  status_tag reservation.status
+                end
+              end
+            end
+
+            strong { link_to 'View All', admin_reservations_path }
+          else
+            strong { 'No reservations today.' }
+          end
+        end
+      end
+
+      column do
+        panel 'Upcoming' do
+          table_for Reservation.upcoming.limit(5) do
+            column :name do |reservation|
+              link_to reservation.name, admin_reservation_path(reservation)
+            end
+            column :number_of_guests
+            column 'Date', :date_time, sortable: :date_time do |reservation|
+              reservation.date_time.strftime('%B %-d, %Y %I:%M %P')
+            end
+            column :status do |reservation|
+              case reservation.status
+              when 'Pending'
+                status_tag reservation.status, class: 'warning'
+              when 'Confirmed'
+                status_tag reservation.status, class: 'ok'
+              when 'Closed'
+                status_tag reservation.status
+              end
+            end
+          end
+
+          strong { link_to 'View All', admin_reservations_path }
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel 'Pending' do
+          table_for Reservation.pending.limit(5) do
+            column :name do |reservation|
+              link_to reservation.name, admin_reservation_path(reservation)
+            end
+            column :number_of_guests
+            column 'Date', :date_time, sortable: :date_time do |reservation|
+              reservation.date_time.strftime('%B %-d, %Y %I:%M %P')
+            end
+            column :status do |reservation|
+              case reservation.status
+              when 'Pending'
+                status_tag reservation.status, class: 'warning'
+              when 'Confirmed'
+                status_tag reservation.status, class: 'ok'
+              when 'Closed'
+                status_tag reservation.status
+              end
+            end
+          end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+          strong { link_to 'View All', admin_reservations_path }
+        end
+      end
+    end
+  end
 end
