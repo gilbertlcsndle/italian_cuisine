f56df7238f120d778d1cb53be2b3de5510bc7ce8
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-03-30 16:41:04 +0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,27 @@
 Reservation.skip_callback(:create, :after, :send_details_to_admin, :send_notification_to_client)
-Reservation.skip_callback(:create, :before, :set_status_to_pending)
 50.times do
   name = Faker::Name.name
   Reservation.create!(name: name,
                       email: "#{name.parameterize(separator: '_')}@example.com",
                       phone: "09#{(0..8).to_a.shuffle[0..8].join}".to_i,
                       number_of_guests: (10..50).step(10).to_a.shuffle.first,
-                      date_time:  Faker::Time.between(DateTime.now + 1, 30.days.from_now),
-                      status: ['Pending', 'Confirmed', 'Closed'].shuffle.first,
+                      date_time:  Faker::Time.between(DateTime.now - 2, 30.days.from_now),
                       message: Faker::Hipster.paragraph)
+end
+
+# close all reservations last two days and today
+this = Date.today
+last_two = Date.today - 2
+
+Reservation.where(date_time: last_two.midnight..this.end_of_day).each do |reservation|
+  reservation.close
+end
+
+# confirm some upcoming reservation
+confirmed_count = (5..Reservation.upcoming.count).to_a.shuffle.first
+
+Reservation.upcoming.order('RANDOM()').limit(confirmed_count).each do |reservation|
+  reservation.confirm(notify: false)
 end
 
 AdminUser.create!(email: 'admin@example.com',
