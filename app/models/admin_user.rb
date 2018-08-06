eaaed4f0d4a4b486279f236bee079bc661a84c24
gilbertlcsndle <gilbert.lacasandile@gmail.com>
2018-08-06 17:50:36 +0800

    Enable confirmable

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,6 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
