97bea0c1be8679506d208cd4ed878129928a7b3e
gilbertlc <gilbert.lacasandile@gmail.com>
2021-07-04 16:04:53 +0800

    fix mailer prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # ActionMailer configs
-  config.action_mailer.default_url_options = { host: 'https://powerful-headland-52058.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://stormy-earth-93721.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
